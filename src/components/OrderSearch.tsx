1dc0918f164b093ca130a55363948c806ba541f2
Anjan KAsi <anjanks@gmail.com>
2024-03-28 14:03:28 +0000

    working with countrychange

--- src/components/OrderSearch.tsx
+++ src/components/OrderSearch.tsx
@@ -23,25 +23,42 @@ const OrderSearch = (): JSX.Element =>{
 
     const [selectedCountry , setSelectedCountry] = useState<string>(uniqueCountries[0])
 
+    let tempArray: PaymentType[] = [];
+    paymentTypeAllData.map(e => {
+        if(e.country === selectedCountry) {
+            tempArray.push(e);
+        } 
+    })
+    
+    if(tempArray.length !== 0){
+        resultOfFilteringPaymenTypeArray = tempArray;
+        //console.log(resultOfFilteringPaymenTypeArray);
+    } else {
+        resultOfFilteringPaymenTypeArray = {...paymentTypeAllData}
+    }
     const changeCountry = (e : ChangeEvent<HTMLSelectElement>) => {
         const option = e.target.options.selectedIndex;
         setSelectedCountry((country) => (uniqueCountries[option]));
-        console.log("Selected country is " + selectedCountry);
-        console.log("change dropdown value is " + uniqueCountries[option]);
+        //Note: once the above line is executed, the code from beginning of the component function is executed all over.
+        // so, anything below is run only if selectedCountry remains the same value i.e state not changed.
+        // intentionally commenting the lines below to prove the point that it is wrong to do any logic when such setter functions
+        // of useState are triggered.
+        // console.log("Selected country is " + selectedCountry);
+        // console.log("change dropdown value is " + uniqueCountries[option]);
        
-        let tempArray: PaymentType[] = [];
-        paymentTypeAllData.map(e => {
-            if(e.country === selectedCountry) {
-                tempArray.push(e);
-            } 
-        })
-        console.log(tempArray);
-        if(tempArray.length !== 0){
-            resultOfFilteringPaymenTypeArray = tempArray;
-            //console.log(resultOfFilteringPaymenTypeArray);
-        } else {
-            resultOfFilteringPaymenTypeArray = {...paymentTypeAllData}
-        }
+        // let tempArray: PaymentType[] = [];
+        // paymentTypeAllData.map(e => {
+        //     if(e.country === selectedCountry) {
+        //         tempArray.push(e);
+        //     } 
+        // })
+        
+        // if(tempArray.length !== 0){
+        //     resultOfFilteringPaymenTypeArray = tempArray;
+        //     //console.log(resultOfFilteringPaymenTypeArray);
+        // } else {
+        //     resultOfFilteringPaymenTypeArray = {...paymentTypeAllData}
+        // }
     }
 
 
